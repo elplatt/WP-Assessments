f7b05ec7616e393c1b292f20d2deffda5ef4e7bc
Shailesh Vedula <vedulashailesh@gmail.com>
2016-10-22 22:02:07 -0400

    First commit

---
+++ FileSystem.py
@@ -0,0 +1,19 @@
+# Project: Identifying shocks in Wikipedia projects
+# Author: Shailesh Vedula
+# Advisors: Dr.Daniel Romero, Dr. Ceren Budak
+# Affiliation: Industrial and Operations Engineering, University of Michigan, Ann Arbor
+# File Name: AbstractIO
+# Date: 10/16/16
+
+class filesystem (object):
+
+    def __init__(self):
+        pass
+
+    Data = "/Users/ishan/Desktop/WikiProject/Data"
+
+    ContFileSystem = "/Users/ishan/Desktop/WikiProject/Time"
+
+    RepeatFileSystem = "/Users/ishan/Desktop/WikiProject/Repeated"
+
+    GapFileSystem = "/Users/ishan/Desktop/WikiProject/Gap"
