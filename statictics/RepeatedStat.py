9020aae52b8440833ceb251d5af47056cb3c2b46
Shailesh Vedula <vedulashailesh@gmail.com>
2016-11-04 14:08:58 -0400

    making the directory a python package

--- statictics/RepeatedStat.py
+++ statictics/RepeatedStat.py
@@ -2,13 +2,7 @@
 # Author: Shailesh Vedula
 # Advisors: Dr.Daniel Romero, Dr. Ceren Budak
 # Affiliation: Industrial and Operations Engineering, University of Michigan, Ann Arbor
-# File Name: __init.py__
+# File Name: RepeatedStat
 # Date: 11/4/16
 
-from csvReader import csvreader
-
-from FileSystem import filesystem
-
-from GradeContinuity import gradecontinuity
-
-from TemporalContinuity import temporalcontinuity
+from continuity.csvReader import csvreader
