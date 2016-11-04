c28d621cde78debf26ff7bb39b533e2a99bfa347
Shailesh Vedula <vedulashailesh@gmail.com>
2016-11-04 13:54:04 -0400

    adding Pacjage __init.py__

---
+++ continuity/__init.py__.py
@@ -0,0 +1,6 @@
+# Project: Identifying shocks in Wikipedia Project
+# Author: Shailesh Vedula
+# Advisors: Dr.Daniel Romero, Dr. Ceren Budak
+# Affiliation: Industrial and Operations Engineering, University of Michigan, Ann Arbor
+# File Name: __init.py__
+# Date: 11/4/16
