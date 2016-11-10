2fe65cde92be207d32150ee18274d2b4ff9d9dde
Shailesh Vedula <vedulashailesh@gmail.com>
2016-11-10 16:14:28 -0500

    __init__.py for spyder package

---
+++ spyder/__init__.py
@@ -0,0 +1,6 @@
+# Project: Identifying shocks in Wikipedia Project
+# Author: Shailesh Vedula
+# Advisors: Dr.Daniel Romero, Dr. Ceren Budak
+# Affiliation: Industrial and Operations Engineering, University of Michigan, Ann Arbor
+# File Name: __init__.py
+# Date: 11/10/16
