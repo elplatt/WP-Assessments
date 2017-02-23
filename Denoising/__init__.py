57aec03be25681cc9d20e64bbe9338bd0dd70029
Edward L Platt <ed@elplatt.com>
2017-02-23 12:21:09 -0500

    Remove unused files.

--- Denoising/__init__.py
+++
@@ -1,6 +0,0 @@
-# Project: Identifying shocks in Wikipedia Project
-# Author: Shailesh Vedula
-# Advisors: Dr.Daniel Romero, Dr. Ceren Budak
-# Affiliation: Industrial and Operations Engineering, University of Michigan, Ann Arbor
-# File Name: __init__.py
-# Date: 11/29/16
